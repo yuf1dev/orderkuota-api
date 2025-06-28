e8c31649fbb2ecc22b4f4627a45587166d93a9ed
YuF1Dev <fikrimaulana85@gmail.com>
2025-06-28 11:04:01 +0700

    Update OrderKuota.php
    Update NAME and CODE Version

--- src/OrderKuota.php
+++ src/OrderKuota.php
@@ -6,15 +6,16 @@ namespace YuF1Dev;
  * [OrderKuota] OrderKuota Api PHP Class (Un-Official)
  * Author : YuF1Dev <https://github.com/yuf1dev>
  * Created at 10-10-2023 00:22
- * Last Modified at 11-10-2023 23:18
+ * Last Modified at 28-06-2025 23:18
  */
 class OrderKuota
 {
     const API_URL = 'https://app.orderkuota.com:443/api/v2';
+    const API_URL_ORDER = 'https://app.orderkuota.com:443/api/v2/order';
     const HOST = 'app.orderkuota.com';
     const USER_AGENT = 'okhttp/4.10.0';
-    const APP_VERSION_NAME = '23.02.01';
-    const APP_VERSION_CODE = '230201';
+    const APP_VERSION_NAME = '25.03.14';
+    const APP_VERSION_CODE = '250314';
     const APP_REG_ID = 'di309HvATsaiCppl5eDpoc:APA91bFUcTOH8h2XHdPRz2qQ5Bezn-3_TaycFcJ5pNLGWpmaxheQP9Ri0E56wLHz0_b1vcss55jbRQXZgc9loSfBdNa5nZJZVMlk7GS1JDMGyFUVvpcwXbMDg8tjKGZAurCGR4kDMDRJ';
 
     private $authToken, $username;
@@ -46,6 +47,14 @@ class OrderKuota
         $payload = "auth_token=" . $this->authToken . "&auth_username=" . $this->username . "&requests%5Bqris_history%5Bjumlah%5D=&requests%5Bqris_history%5D%5Bjenis%5D=" . $type . "&requests%5Bqris_history%5D%5Bpage%5D=1&requests%5Bqris_history%5D%5Bdari_tanggal%5D=&requests%5Bqris_history%5D%5Bke_tanggal%5D=&requests%5Bqris_history%5D%5Bketerangan%5D=&requests%5B0%5D=account&app_version_name=" . self::APP_VERSION_NAME . "&app_version_code=" . self::APP_VERSION_CODE . "&app_reg_id=" . self::APP_REG_ID . "";
         return self::Request("POST", self::API_URL . '/get', $payload, true);
     }
+
+    public function withdrawalQris($amount = '')
+    {
+        $payload = "app_reg_id=" . self::APP_REG_ID . "&app_version_code=" . self::APP_VERSION_CODE . "&auth_username=" . $this->username . "&requests[qris_withdraw][amount]=" . $amount . "&auth_token=" . $this->authToken . "&app_version_name=" . self::APP_VERSION_NAME . "";
+        return self::Request("POST", self::API_URL . '/get', $payload, true);
+    }
+
+
     protected function buildHeaders()
     {
         $headers = array(
